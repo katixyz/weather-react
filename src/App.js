014b343809a99425ec4828a35e91dd875539cac0
katixyz <139153535+katixyz@users.noreply.github.com>
2024-05-05 13:52:50 +0200

    Added a weather app, which was created before using CodeSandbox

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">React Weather App</h1>
-      <Weather city="Lisbon" />
+      <Weather />
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function searchWeather(props) {
-  function displayWeather(response) {
-    alert(`The weather in ${props.city} is ${response.data.main.temp} °C.`);
+import "./Weather.css";
+
+export default function SearchWeather() {
+  let [city, setCity] = useState("");
+  let [weather, setWeather] = useState({});
+  let [loaded, setLoaded] = useState(false);
+
+  function showClimateValues(response) {
+    setLoaded(true);
+    setWeather({
+      name: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+    console.log(response.data);
   }
 
-  let apiKey = "094780c710fa4efd669f0df8c3991927";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(displayWeather);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "094780c710fa4efd669f0df8c3991927";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(showClimateValues);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form className="search-city" onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="How is the weather in..."
+        className="input-city"
+        onChange={updateCity}
+      />
+      <button type="Submit" className="search-city-button">
+        Search{" "}
+      </button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <div className="climate">
+          <h1>{weather.name}</h1>
+          <img src={weather.icon} alt="Sunny" className="icon" />
+          <h2 className="temperature">
+            {Math.round(weather.temperature)}
+            <span className="units">°C</span>
+          </h2>
+
+          <ul className="climate-values">
+            <li>Humidity: {weather.humidity} %</li>
+            <li>Wind speed: {Math.round(weather.wind)} km/h</li>
+            <li>{weather.description}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
